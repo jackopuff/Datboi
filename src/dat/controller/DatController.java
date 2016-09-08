5ab3c5834a106d085d7878d23944a875f6c76001
jackopuff <jcar8281@740s-pl-128.local>
2016-09-08 12:42:41 -0600

    added user input, variables, displayed text.

--- src/dat/controller/DatController.java
+++ src/dat/controller/DatController.java
@@ -1,8 +1,28 @@
 package dat.controller;
 
-public class DatController {
+import java.util.Scanner;
+
+public class DatController
+{
+	private String myName;
+	private Scanner readKeyboardInput;
+	public DatController()
+	{
+		myName = "Jackson Carter";
+		readKeyboardInput = new Scanner(System.in);
+	}
+	
 	public void start()
 	{
 		System.out.println("suh dude");
+		System.out.println(myName);
+		askQuestions();
+	}
+	
+	private void askQuestions()
+	{
+		System.out.println("What is your name?");
+		String userInput = readKeyboardInput.next();
+		System.out.println("You said " + userInput);
 	}
 }
