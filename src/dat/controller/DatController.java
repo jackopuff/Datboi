7e786c3dc2dab6092b41c088932de1e65b600e0c
jackopuff <jcar8281@740s-pl-128.local>
2016-09-12 13:18:32 -0600

    added several questions

--- src/dat/controller/DatController.java
+++ src/dat/controller/DatController.java
@@ -22,9 +22,55 @@ public class DatController
 	private void askQuestions()
 	{
 		System.out.println("What is your name?");
-		String userInput = readKeyboardInput.next();
+		String userInput = readKeyboardInput.nextLine();
 		System.out.println("You said " + userInput);
-		System.out.println("a");
-		System.out.println("c");
+
+		
+		System.out.println("How old are you?");
+		Integer ageInput = readKeyboardInput.nextInt();
+		System.out.println("you are " + ageInput + " years old");
+		readKeyboardInput.nextLine();
+
+		System.out.println("how much does a Wendy's Dave's Double cost?");
+		Double priceInput = readKeyboardInput.nextDouble();
+		
+		if (priceInput == 6.19) 
+			{
+				System.out.println("you are correct");
+			}
+		else
+			{
+				System.out.println("No, a Wendy's Dave's Double costs $6.19");
+			}
+		readKeyboardInput.nextLine();
+
+		System.out.println("What is your mother's maiden name?");
+		String motherInput = readKeyboardInput.next();
+		System.out.println("Your mother's maiden name is " + motherInput);
+		readKeyboardInput.nextLine(); // clears remainder of the text.
+	
+		
+		System.out.println("Who is your favorite president?");
+		String presidentInput = readKeyboardInput.nextLine();
+		System.out.println("Your favorite president is " + presidentInput);
+	
+		
+		System.out.println("what was your first pet's name?");
+		String petInput = readKeyboardInput.next();
+		System.out.println("Your pet's name is " +petInput);
+		readKeyboardInput.nextLine();
+	
+		
+		System.out.println("Did you brush your teeth today? (Y/N");
+		String teethInput = readKeyboardInput.next();
+		if (teethInput.equals ("Y") || teethInput.equals("Yes") || teethInput.equals("yes") || teethInput.equals("y"))
+			{
+				System.out.println("I'm Glad");
+			}
+		else
+			{
+				System.out.println("That's nasty");
+			}
+		readKeyboardInput.nextLine();
 	}
 }
